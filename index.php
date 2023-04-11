9bbd9985c4cfb88a503ff7703a6c069e09e143db
unknown <fixik2019@gmail.com>
2023-04-11 11:43:55 +0300

    save

--- index.php
+++ index.php
@@ -1,39 +1,42 @@
-<?php include 'header.php'?>
-<?php include 'db.php'?>
-<h2 id="main_sub_title">All students</h2>
-<table class="table table-hover table-bordered">
-    <thead>
-    <tr>
-        <th>ID</th>
-        <th>Фамилия</th>
-        <th>Имя</th>
-        <th>Возраст</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    $query = "SELECT * FROM `students`";
-    $result = mysqli_query($connection, $query);
+<?php include 'header.php' ?>
+<?php include 'db.php' ?>
+    <div class="wrap-1">
+        <div class="wrapp-1__item">
+            <h2 id="main_sub_title">All students</h2>
+        </div>
+        <div class="wrapp-1__item">
+            <button class="btn btn-primary">Добавить студента</button>
+        </div>
+    </div>
+    <table class="table table-hover table-bordered">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Фамилия</th>
+            <th>Имя</th>
+            <th>Возраст</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $query = "SELECT * FROM `students`";
+        $result = mysqli_query($connection, $query);
 
-    if(!$result){
-        die("Query failed".mysqli_error());
-    }
-    else{
-
-        while($row = mysqli_fetch_assoc($result)){
-            ?>
-            <tr>
-                <td><?=$row['id']?></td>
-                <td><?=$row['first_name']?></td>
-                <td><?=$row['last_name']?></td>
-                <td><?=$row['age']?></td>
-            </tr>
-            <?php
+        if (!$result) {
+            die("Query failed" . mysqli_error());
+        } else {
+            while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+                <tr>
+                    <td><?= $row['id'] ?></td>
+                    <td><?= $row['first_name'] ?></td>
+                    <td><?= $row['last_name'] ?></td>
+                    <td><?= $row['age'] ?></td>
+                </tr>
+                <?php
+            }
         }
-
-    }
-
-    ?>
-    </tbody>
-</table>
-<?php include 'footer.php'?>
+        ?>
+        </tbody>
+    </table>
+<?php include 'footer.php' ?>
