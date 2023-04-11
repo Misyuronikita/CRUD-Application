db4f7d2963e49975092e9f031affbf176fb43732
unknown <fixik2019@gmail.com>
2023-04-11 11:01:34 +0300

    save

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include 'header.php'?>
+<h2 id="main_sub_title">All students</h2>
+<table class="table table-hover table-bordered">
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Фамилия</th>
+        <th>Имя</th>
+        <th>Возраст</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>1</td>
+        <td>Мисюро</td>
+        <td>Никита</td>
+        <td>19</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Станиш</td>
+        <td>Эрик</td>
+        <td>19</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Илья</td>
+        <td>Воробьев</td>
+        <td>19</td>
+    </tr>
+    </tbody>
+</table>
+<?php include 'footer.php'?>
